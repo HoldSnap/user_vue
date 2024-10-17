9b54babd887d6cc9e18f0c3382a67d17f86ffd33
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-10-17 14:54:18 +0300

    feat(component): update SortOptions

--- src/components/SortOptions.vue
+++ src/components/SortOptions.vue
@@ -4,30 +4,17 @@
       <span class="sort__label">Сортировка:</span>
       <button class="sort__option" @click="sortBy('registration_date')">
         Дата регистрации
-        <span v-if="sortField === 'registration_date'">{{
-          sortDirection === 'asc' ? '↑' : '↓'
-        }}</span>
       </button>
-      <button class="sort__option" @click="sortBy('rating')">
-        Рейтинг
-        <span v-if="sortField === 'rating'">{{
-          sortDirection === 'asc' ? '↑' : '↓'
-        }}</span>
-      </button>
+      <button class="sort__option" @click="sortBy('rating')">Рейтинг</button>
     </div>
   </div>
 </template>
 
 <script setup>
 'use strict'
-
-import { computed } from 'vue'
 import { useStore } from 'vuex'
 
 const store = useStore()
-
-const sortField = computed(() => store.state.sortField)
-const sortDirection = computed(() => store.state.sortDirection)
 
 const sortBy = field => {
   store.dispatch('sortUsers', field)
