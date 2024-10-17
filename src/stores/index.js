5a6a07e00082899530a79cad31856a7e4741f6ae
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-10-17 14:29:18 +0300

    feat(store): create VUEX store for users

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,10 +1,40 @@
+'use strict'
+
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 const store = createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  getters: {},
+  state: {
+    users: [],
+  },
+  mutations: {
+    SET_USERS(state, users) {
+      state.users = users
+    },
+    DELETE_USER(state, userId) {
+      state.users = state.users.filter(user => user.id !== userId)
+    },
+  },
+  actions: {
+    async fetchUsers({ commit }) {
+      try {
+        const response = await axios.get(
+          'https://5ebbb8e5f2cfeb001697d05c.mockapi.io/users',
+        )
+        commit('SET_USERS', response.data)
+      } catch (error) {
+        console.error('Ошибка при загрузке пользователей:', error)
+      }
+    },
+    deleteUser({ commit }, userId) {
+      commit('DELETE_USER', userId)
+    },
+  },
+  getters: {
+    allUsers(state) {
+      return state.users
+    },
+  },
 })
 
 export default store
--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="user-list">
     <UserListHeader />
-
     <SearchFilter />
-
     <SortOptions />
-
     <UserTable />
   </div>
 </template>
+
+<script setup>
+'use strict'
+
+import UserListHeader from '../components/UserListHeader.vue'
+import SearchFilter from '../components/SearchFilter.vue'
+import SortOptions from '../components/SortOptions.vue'
+import UserTable from '../components/UserTable.vue'
+</script>
+
+<style scoped></style>
