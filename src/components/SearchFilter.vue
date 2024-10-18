9991eb046272d5f46d7cc53010f1c6aaa009d3b9
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-10-18 11:55:59 +0300

    style: update SearchFilter

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="user-list__search">
     <div class="search">
+      <svg
+        class="search__icon"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <circle
+          cx="10"
+          cy="10"
+          r="8"
+          stroke="#949494"
+          stroke-width="2"
+          fill="none"
+        ></circle>
+        <line
+          x1="15"
+          y1="15"
+          x2="21"
+          y2="21"
+          stroke="#949494"
+          stroke-width="2"
+        ></line>
+      </svg>
       <input
         type="text"
         placeholder="Поиск по имени или e-mail"
@@ -44,11 +68,48 @@ const clearFilters = () => {
 <style scoped lang="scss">
 @import '../assets/variables';
 
-.user-list__search {
+.search {
+  position: relative;
   width: 100%;
-  box-shadow: 0px 18px 15px 0px #94949426;
-  height: 102px;
+  height: 100%;
+}
+
+.search__icon {
+  position: absolute;
+  left: 16px;
+  top: 30%;
+  transform: translateY(-50%);
+  pointer-events: none;
+}
+
+.search__input {
+  width: 100%;
+  height: 44px;
+  padding: 12px 16px 12px 44px;
+  border: 1px solid #e5e5e5;
+  border-radius: 4px;
+  background-color: #f1f1f1;
+  color: #333;
+}
+
+.search__clear {
+  margin-left: 16px;
+  padding: 12px;
+  background: none;
+  border: none;
+  font-size: 16px;
+  color: #333;
+  cursor: pointer;
+}
+
+.user-list__search {
   display: flex;
   align-items: center;
+  padding: 16px;
+  padding-right: 5%;
+  box-shadow: 0px 18px 15px 0px #94949426;
+  background-color: #fff;
+  border-radius: 7px;
+  height: 102px;
 }
 </style>
